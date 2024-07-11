5744f1c70067565b57127c90190d322138dbff83
Rubi Henjaya <rubi.henjaya@gmail.com>
2024-07-11 20:09:43 +0700

    prisma schema

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,8 @@
+export async function GET(request: Request) {
+    // const { searchParams } = new URL(request.url)
+    // const id = searchParams.get('id')
+
+    
+   
+    // return Response.json({ product })
+  }
